c2952651a54a449182b0249ccde64627ec789336
KyeCook <acfanatics@hotmail.com>
2016-05-10 17:03:31 +1000

    Updated and tried to make 'Add Items' GUI work independently, didn't work...

--- KivyTesting.py
+++ KivyTesting.py
@@ -11,27 +11,31 @@ from kivy.lang import Builder
 class Converter(App):
     def build(self):
         self.title = "Equipment Hire"
-        self.root = Builder.load_file('gui_addItems.kv')
-        # self.root = Builder.load_file('gui_mainMenu.kv')
+        # self.root = Builder.load_file('gui_addItems.kv')
+        self.root = Builder.load_file('gui_mainMenu.kv')
         return self.root
 
-    def handle_convert(self):
-        miles = float(self.root.ids.input_miles.text)
-        kilometers = miles/0.62137
-        if kilometers > 1:
-            is_plural = "s"
-        else:
-            is_plural = ""
-        self.root.ids.output_label.text = "{:.2f}km{}".format(kilometers, is_plural)
+    def handle_add_item(self):
+        self.root = Builder.load_file('gui_addItems.kv')
+
+        ## ?????????????????????????????
+
+        # miles = float(self.root.ids.input_miles.text)
+        # kilometers = miles/0.62137
+        # if kilometers > 1:
+        #     is_plural = "s"
+        # else:
+        #     is_plural = ""
+        # self.root.ids.output_label.text = "{:.2f}km{}".format(kilometers, is_plural)
 
     def handle_add(self):
         mile_plus1 = float(self.root.ids.input_miles.text)
-        mile_plus1 +=1
+        mile_plus1 += 1
         self.root.ids.input_miles.text = str(mile_plus1)
 
     def handle_take(self):
         mile_minus1 = float(self.root.ids.input_miles.text)
-        mile_minus1 -=1
+        mile_minus1 -= 1
         self.root.ids.input_miles.text = str(mile_minus1)
 
 
